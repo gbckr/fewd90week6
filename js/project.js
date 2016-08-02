117d9ffe5d7372f06aff483643b0f75aef62bb78
caterina <carmenter@CArmenter-MacBook-Pro.local>
2016-08-02 09:34:11 -0400

    not sure what I'm doing honestly
    nothing is working :’(

---
+++ js/project.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+ $("nav a).click(function(){ 
+ 	$("nav a.current").removeClass("current");
+ 	$(this).addClass("current");
+ 	var lang = $(this).attr("data-ipsum");	
+	$(".ipsum").attr("id", lang);
+});
+ $("#submit").click(function(){
+  $(this).fadeOut();
+  $(this).hide();
+})
